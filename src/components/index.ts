4146e3c790a8bdb69772161551495c3e2374a6d6
David Yappeter <davidyap11les@gmail.com>
2022-08-24 05:02:34 +0000

    fix: add missing Typography from export

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ import Button from "./Button/index";
 import Dialog from "./Dialog/index";
 import GlitchText from "./GlitchText/index";
 import Slider from "./Slider";
+import Typography from "./Typography";
 
-export { Button, Dialog, GlitchText, Slider };
+export { Button, Dialog, GlitchText, Slider, Typography };
